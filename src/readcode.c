f194e1e917893d2eb1a037d259e947d18e449fd8
Ryan Park <juny1905@naver.com>
2017-11-07 05:40:58 -0800

    Implementation of Linked List for Paragraph (Incompleted)

--- src/readcode.c
+++ src/readcode.c
@@ -3,13 +3,48 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-
-#include <stdio.h>
 #define SZ_BUF 1024
 
-// Test for inline reading
+typedef struct *LINE
+{
+	LINE* NEXT;
+	char *text;
+	short type;
+}LINE;
+
+// Test for inline reading and stack
+LINE* HEAD = 0;
+
+void addToText(char *feed, short type)
+{
+	LINE *NEW = (LINE *)malloc(sizeof(LINE));
+	NEW->NEXT = 0; NEW->text = feed;
+	NEW->type = type;
+	if(HEAD -> NEXT = 0)
+	{
+		HEAD = NEW;
+	}
+	else
+	{
+		LINE *CUR = HEAD;
+		while(CUR->NEXT != 0)
+		{
+			CUR = CUR->NEXT;
+		}
+		CUR->NEXT = NEW;
+	}
+}
+void displayText(void) // For testing.
+{
+	LINE *CUR = HEAD;
+	while(CUR->NEXT != 0)
+	{
+		printf("%s | %d",CUR->text,CUR->type);
+		CUR = CUR->NEXT;
+	}		
+}
 char buf[SZ_BUF];
-char *inlineRead(char *filename)
+char *ReadFile(char *filename)
 {
 	int fd;
 	fd = open(filename,O_RDONLY);
@@ -20,11 +55,22 @@ char *inlineRead(char *filename)
 		return NULL;
 	}	
 	
-	while(read(fd, buf, SZ_BUF));
+	while(nb = read(fd, buf, SZ_BUF));
 
 	close(fd);
 
 	return buf;
+}
+void *SeprateFile(char *text)
+{
+	int i;
+	for(i=0;text[i]!=0;i++)
+	{
+		if(text[i]=='\' && text[i+1]=='n')
+		{
+			
+		}
+	}
 }
 int main(int argc, char *argv[])
 {
@@ -34,7 +80,7 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 	
-	char *text = inlineRead(argv[1]);
+	char *text = ReadFile(argv[1]);
 	write(0, text, SZ_BUF);
 	return 0;
 	
