efa2397a57b288136f50bc2696b6160b4ec9e3ee
starpia <starpia@mail.com>
2017-11-29 13:34:35 +0900

    added printFrame Function in ui_main.c

--- src/ui/ui_main.c
+++ src/ui/ui_main.c
@@ -1,21 +1,65 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <ncurses.h>
+#include <curses.h>
 
 void init_ui(void)
 {
 	initscr();
+	start_color();
 	raw();
 	keypad(stdscr, TRUE);
 	noecho();
 	return;
 }
 
+void printFrame(int _maxRow, int _maxCol)
+{
+	int half_maxRow = _maxRow/2;
+	int half_maxCol = _maxCol/2;
+	int midRow = _maxRow-5;
+
+	for(int i=0; i<_maxRow; i++)
+	{
+		for(int j=0; j<_maxCol; j++)
+		{
+			if( ((i == 0) || (i == midRow)) && \
+			(j != half_maxCol) && (j < (_maxCol-1)) && (j > 0) )
+			{
+				mvaddch(i,j,'-'|A_BOLD);
+			}
+			else if( ((i == 0) || (i == midRow)) )
+			{
+				mvaddch(i,j,'+'|A_BOLD);
+			}
+			else if( (i > 0) && (i < midRow) && \
+			((j==0) || (j==half_maxCol) || (j==(_maxCol-1))))
+			{
+				mvaddch(i,j,'|'|A_BOLD);
+			}
+			else if( ((i>(midRow-1)) && (i<(_maxRow-1))) && \
+			((j==0) || (j==(_maxCol-1))))
+			{
+				mvaddch(i,j,'|'|A_BOLD);
+			}
+			else if( (i == (_maxRow-1)) && \
+			((j==0) || (j==(_maxCol-1))))
+			{
+				mvaddch(i,j,'+'|A_BOLD);
+			}
+			else if( (i == (_maxRow-1)) )
+			{
+				mvaddch(i,j,'-'|A_BOLD);
+			}
+		}
+	}
+}
+
 int main(void)
 {
 	int key = -1;
-	int maxCol = 0;
 	int maxRow = 0;
+	int maxCol = 0;
 
 	/* Curses Initializations */
 	init_ui();
@@ -23,11 +67,14 @@ int main(void)
 	/* Check the Console size */
 	getmaxyx(stdscr,maxRow,maxCol);
 
-	printf("%d %d\n",maxRow,maxCol);
+	/* Set the Background And Fonts Color */
+	assume_default_colors(COLOR_WHITE,COLOR_BLACK);
+	
+	printFrame(maxRow, maxCol);
 
-	while(TRUE)
+	while((key = getch()) != '#')
 	{
-		key = getch();
+		
 	}
 
 	/* Exit */
