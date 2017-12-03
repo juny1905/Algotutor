7e65acd28a3aeaa3e6ec4aa410de46d1c9c73ec1
Ryan Park <juny1905@naver.com>
2017-12-02 22:59:08 -0800

    Set test environment

--- src/code_view_side/code_view_test.c
+++ src/code_view_side/code_view_test.c
@@ -1,10 +1,10 @@
 #include "readcode.h"
+#include "viewcode.h"
 
 int main(void)
 {
 	LINE *TEST = (LINE *)malloc(sizeof(LINE));
 	TEST = ReadFile("../../doc/test.txt");
-	displayText(TEST);
-
+	printPara(10,10,10,10,TEST);
 	return 0;	
 }
