0f251666d5d16f8dd17bf5f88822a366c5da1bb2
Ryan Park <juny1905@naver.com>
2017-12-03 03:58:43 -0800

    Final commit before merge

--- src/code_view_side/code_view_test.c
+++ src/code_view_side/code_view_test.c
@@ -5,7 +5,7 @@ int main(void)
 {
 	LINE *TEST = (LINE *)malloc(sizeof(LINE));
 	TEST = ReadFile("../../doc/Chap1-printf/printf.txt");
-	printPara(10,10,30,20,TEST);
+	printPara(10,10,30,5,TEST);
 	// displayText(TEST);	
 	return 0;	
 }
