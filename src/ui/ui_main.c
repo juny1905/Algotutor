1a40eb7ba48fb0a4c3f919dd82ea07157b0ad5fe
starpia <starpia@mail.com>
2017-12-03 16:51:46 +0900

    finish the creat_direcotry function

--- src/ui/ui_main.c
+++ src/ui/ui_main.c
@@ -19,8 +19,8 @@ struct category
 {
 	char *cat_name;
 	unsigned char action;
-	struct category *cat_bottom;
-	struct category *cat_next;	
+	struct category *bottom;
+	struct category *next;	
 };
 
 void init_ui(void)
@@ -128,20 +128,19 @@ void mvaddstr_att(int _y, int _x, const char *_str, int _att)
 	return;
 }
 
-void showMenu(int _maxRow, int _maxCol, int _menuCur, int _categoryNum, char *_menu[][5])
+void selectMenu(int _maxRow, int _maxCol, int _menuCur, struct category *_cat_head)
 {
-	/*
-	char *menu[5][5] = {
-		{"Algotutor - v.1.0.0", "1. Start", "2. End", 0,0},
-		{"Select category","1. scanf and printf","2. if","3. for loop","4. nested for loop"},
-		{0,0,0,0,0},
-		{0,0,0,0,0},
-		{0,0,0,0,0}
-	};
-	*/
-
+	struct category *cur = _cat_head;
+	
 	clearWorkspace(_maxRow,_maxCol,CODE_VIEW_PART);
 	
+	for(int i=0; i<(_menuCur+1); i++)
+	{
+		cur = cur->bottom;
+	}
+
+	if(cur->action == 0)
+
 	//mvadd_wch(4+(_menuCur*2),3,0x25B6);
 	mvaddch(4+(_menuCur*2),3,'>');
 
@@ -166,7 +165,7 @@ int checkMenuLength(int _categoryNum, char *_menu[][5])
 
 }
 
-void read_directory(char *_doc, struct category *_cat_head)
+void create_directory(char *_doc, struct category *_cat_head)
 {
 	
 	DIR *dir = opendir(_doc);
@@ -177,9 +176,6 @@ void read_directory(char *_doc, struct category *_cat_head)
 	struct category *cur = _cat_head;
 	int i = 0;
 
-	/* read category */
-	dirs = readdir(dir);
-	
 	/* root category */
 	while(temp[i] != 0)
 	{	
@@ -191,54 +187,65 @@ void read_directory(char *_doc, struct category *_cat_head)
 			else if(i == 1)
 				cur->action = 1; // another category
 			else if(i == 2)
-				cur->action = -1; // exit program
+				cur->action = -2; // exit program
 
-			cur->bottom = (struct category *)malloc(sizeof(struct category));
 			cur->next = (struct category *)malloc(sizeof(struct category));
 			cur->next->cat_name = (char *)malloc(strlen(temp[i]+1));
 			strcpy(cur->next->cat_name, temp[i]);
+			cur->next->action = 0;
 			cur->next->next = NULL;
 			cur->next->bottom = NULL;
 
+			cur->bottom = (struct category *)malloc(sizeof(struct category));
 			cur = cur->bottom;
 			cur->next = NULL;
 			cur->bottom = NULL;
+
+			i++;
+
 	}
 
-	/* another category */
+	/* read another category */
 	cur = _cat_head;
 	while(cur->action != 1)
 	{
 			cur = cur->bottom;
 	}
+	cur = cur->next;
 
-	cur->next = (struct category *)malloc(sizeof(struct category));
-	
-
-	head->cat_name = (char *)malloc(strlen(dirs->d_name)+1);
-	strcpy(head->cat_name,dirs->d_name);
-	head->next = NULL;
-	//mvaddstr(10,2,head->cat_name);
-
+	while((dirs = readdir(dir)) != NULL)	
 	{
-		struct menu *cur = head;
-
-		while((dirs = readdir(dir)) != NULL)
+		if((strcmp(dirs->d_name,".") != 0) &&\
+		   (strcmp(dirs->d_name,"..") != 0))
 		{
-				if((strcmp(dirs->d_name,".") != 0) &&\
-				   (strcmp(dirs->d_name,"..") != 0))
-				{
-					cur->next = (struct menu *)malloc(sizeof(struct menu));
-					cur->next->cat_name = (char *)malloc(strlen(dirs->d_name)+1);
-					strcpy(cur->cat_name,dirs->d_name);
-					cur->next->next = NULL;
-					cur = cur->next;
-				}
-			
+			continue;
 		}
 
-		//mvaddstr(20,2,cur->cat_name);
+		if(cur->cat_name == NULL)
+		{
+			cur->cat_name = (char *)malloc(strlen(dirs->d_name)+1);
+			strcpy(cur->cat_name, dirs->d_name);
+			cur->action = 1; 
+		}
+
+		if(cur->bottom == NULL)
+		{
+			cur->bottom = (struct category *)malloc(sizeof(struct category));
+			cur->bottom->action = 0;
+			cur->bottom->cat_name = NULL;
+			cur->bottom->next = NULL;
+			cur->bottom->bottom = NULL;
+		}
+
+		cur = cur->bottom;
 	}
+
+	cur->cat_name = (char *)malloc(strlen("Back")+1);
+	strcpy(cur->cat_name, "Back");
+	cur->action = -1; 
+
+	return;
+
 }
 
 
@@ -252,11 +259,13 @@ int main(void)
 	int maxCat = 5;
 	int menuCur = 0;
 	int maxMenu = 5;	
-
+	
+	struct category *cur = NULL;
+	struct category *prev = NULL;
 	struct category *cat_head = (struct category *)malloc(sizeof(struct category));
 	cat_head->action = 0;
-	cat_head->cat_bottom = NULL;
-	cat_head->cat_next = NULL;
+	cat_head->bottom = NULL;
+	cat_head->next = NULL;
 
 	/* Curses Initializations */
 	init_ui();
@@ -269,26 +278,15 @@ int main(void)
 	
 	/* Show up interface Frame */
 	printFrame(maxRow, maxCol);
-	//showMenu(maxRow,maxCol,menuCur,categoryNum,menu);
+	showMenu(maxRow,maxCol,menuCur,cat_head);
 	
-
-
-	/* testing */
-	/*	
-	{
-		int i = 0;
-
-		while(head != NULL)
-		{
-			mvaddstr(i,2,head->cat_name);
-			head = head->next;
-			i++;
-		}
-	}
-	*/
-
+	/*  */
+	create_directory("../../doc/",cat_head);
+	cur = cat_head;
+	
 	while((key = getch()) != 27)
 	{
+
 		switch(key)
 		{
 			case KEY_UP:	if((menuCur - 1) < 0)
@@ -304,12 +302,15 @@ int main(void)
 			case KEY_DOWN:	menuCur = (menuCur + 1)%maxMenu;
 					break;
 
-			case '\n':	categoryNum = (categoryNum+1)%maxCat;
+			case '\n':
+					prev = cur;
+					showMenu(maxRow,maxCol,menuCur,cur);					
+					categoryNum = (categoryNum+1)%maxCat;
 					menuCur = 0;
 					break;
 		}
 
-		//showMenu(maxRow,maxCol,menuCur,categoryNum,menu);
+	
 
 		// for debugging
 		if( key == 'a' )
