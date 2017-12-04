7ed465c4f95e634922b8f34b3dcda3dc2e170ab9
starpia <starpia@mail.com>
2017-12-04 20:21:57 +0900

    using pthread

--- src/main.c
+++ src/main.c
@@ -1,12 +1,56 @@
 #include "console.h"
+#include <pthread.h>
+#include <stdlib.h>
 
+#define MAX_THREADS 2
+
+int xlimit = 0;
+int ylimit = 0;
+
+void *user_interface(void *_arg)
+{
+	ui_main(&xlimit,&ylimit);
+	pthread_exit(NULL);
+}
+
+void *code_view(void *_arg)
+{
+	pthread_exit(NULL);
+}
 
 int main(int argc, char *argv)
 {
+	pthread_t threadPool[MAX_THREADS];
+
 	int xlimit = 0;
 	int ylimit = 0;
 
-	ui_main(&xlimit,&ylimit);
+	/* create threads */
+	for(int i=0; i<MAX_THREADS; i++)
+	{
+		int result = 0;
+		if( i == 0 )
+		{
+			result = pthread_create(&threadPool, NULL,\
+				user_interface, NULL);
+		}
+		else if( i == 1 )
+		{
+			result = pthread_create(&threadPool, NULL,\
+				code_view, NULL);
+		}
+
+		if(result != 0)
+		{
+			perror("pthread_create : ");
+			exit(1);
+		}
+	}
+
+	for(int i=0; i<MAX_THREADS; i++)
+	{
+		pthread_join(threadPool[i], NULL);
+	}
 
 	return 0;
 }
